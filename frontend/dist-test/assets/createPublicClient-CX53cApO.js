60a6ade7ca01eefba6e1a0ef6da144001b46da43
compu <inux2012@gmail.com>
2026-05-23 20:59:10 -0300

    remove dist-test

--- frontend/dist-test/assets/createPublicClient-CX53cApO.js
+++
@@ -1,1 +0,0 @@
-import{d as i,p as n}from"./index-CfjC4bUz.js";function u(t){const{key:e="public",name:c="Public Client"}=t;return i({...t,key:e,name:c,type:"publicClient"}).extend(n)}export{u as c};
