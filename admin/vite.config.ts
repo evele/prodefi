ff339dabb37ae7b9364fdead7cf60917046ab239
Eric <inux2012@gmail.com>
2026-05-26 19:31:27 -0300

    small config fixes

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoRoot = '/home/compu/Proyectos/prodefi'
-
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      allow: [repoRoot],
-    },
-  },
 })
